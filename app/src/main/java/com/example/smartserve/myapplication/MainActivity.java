8c3f898642135dab1fe1203b92110106d078be67
SumanpreetLubana <preetlubana5397@gmail.com>
2018-09-10 11:24:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/smartserve/myapplication/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.smartserve.myapplication;
+
+import android.app.Activity;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
+public class MainActivity extends Activity implements BeaconConsumer {
+
+    TextView mTvDistance;
+
+    protected static final String TAG = "RangingActivity";
+    private BeaconManager beaconManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        mTvDistance = (TextView) findViewById(R.id.tv_distance);
+
+        beaconManager.bind(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (beaconManager.isBound(this)) beaconManager.setBackgroundMode(true);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (beaconManager.isBound(this)) beaconManager.setBackgroundMode(false);
+    }
+
+
+
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    Log.d(TAG, "didRangeBeaconsInRegion called with beacon count:  "+beacons.size());
+                    Beacon firstBeacon = beacons.iterator().next();
+                    mTvDistance.setText("Distance is: "+ firstBeacon.getDistance());
+                    logToDisplay("The first beacon " + firstBeacon.toString() + " is about " + firstBeacon.getDistance() + " meters away.");
+                }
+            }
+
+        });
+
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {   }
+    }
+
+    public void logToDisplay(String s) {
+    }
+}
